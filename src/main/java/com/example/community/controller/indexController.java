933781298402039ca398d5a2289aa4c07c78241b
Lamin-cj <104662529+Lamin-cj@users.noreply.github.com>
2022-04-30 23:00:59 +0800

    add index and bootstrap

--- src/main/java/com/example/community/controller/indexController.java
+++ src/main/java/com/example/community/controller/indexController.java
@@ -11,7 +11,7 @@ public class HelloController {
     public  String hello(@RequestParam(name = "name") String name, Model model){
 //        model.addAttribute("name",name);
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
 
     }
 }
